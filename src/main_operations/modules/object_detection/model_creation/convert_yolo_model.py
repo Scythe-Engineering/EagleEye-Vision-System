f0473090d7c64b5510883385709088b1382198b1
DarkEden-coding <darkedenc9@gmail.com>
2025-10-30 19:00:37 -0400

    OPSET 11??????????

--- src/main_operations/modules/object_detection/model_creation/convert_yolo_model.py
+++ src/main_operations/modules/object_detection/model_creation/convert_yolo_model.py
@@ -7,7 +7,7 @@ def load_and_convert_yolo_model() -> None:
     """Load a YOLO model from user input and convert it to ONNX format with opset 17."""
     yolo_model_path = input("Enter the path to the YOLO model file: ")
     yolo_model = YOLO(yolo_model_path)
-    yolo_model.export(format="onnx")
+    yolo_model.export(format="onnx", opset=11)
 
 
 if __name__ == "__main__":
